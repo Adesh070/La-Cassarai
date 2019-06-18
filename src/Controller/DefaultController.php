84aeda3723dc5cec9e1f35c422c66dfd3f84b169
Adesh070 <49236726+Adesh070@users.noreply.github.com>
2019-06-18 16:27:05 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("", name="Default")
+     */
+    public function index()
+    {
+        return $this->render('Default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
